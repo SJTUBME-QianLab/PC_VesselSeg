4b616d4249cc70768b381cf4e07a5c7eadc7d489
SJTU-QianLab <62273199+SJTUBME-QianLab@users.noreply.github.com>
2024-07-09 20:53:03 +0800

    Delete augmentation directory

--- augmentation/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3.5
-# -*- coding: utf-8 -*-
-# @Time    : 2021/5/5 16:34
-# @Author  : Dirk Li
-# @Email   : junli.dirk@gmail.com
-# @File    : __init__.py.py
-# @Software: PyCharm,Windows10
-# @Hardware: 32G-RAM,Intel-i7-7700k,GTX-1080
--- augmentation/volumentations/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .augmentations.functionals import *
-from .augmentations.transforms import *
-from .core.composition import *
-from .core.transforms_interface import *
